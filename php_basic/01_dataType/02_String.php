f14fe27953e89cebd3556203d88eaa51def74dbb
okt0312 <okt0312@naver.com>
2020-10-04 02:18:08 +0900

    자료형

---
+++ php_basic/01_dataType/02_String.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String & String Operator</title>
+</head>
+<body>
+    <h1>String & String Operator</h1>
+    <?
+        echo "Hello \"w\"orld";
+    ?>
+    <h2>concatenation operator</h2>
+    <?
+        echo "Hello "."world"; // 결합연산자
+    ?>
+    <h2>String length function</h2>
+    <?
+        echo strlen("Hello world"); // 문자열 갯수 확인
+    ?>
+</body>
+</html>
