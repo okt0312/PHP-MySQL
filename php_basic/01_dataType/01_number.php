25f02ff8eef55fe8cb2547237803f803bdb5fb73
okt0312 <okt0312@naver.com>
2020-10-04 01:44:48 +0900

    자료형-연산자

---
+++ php_basic/01_dataType/01_number.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number & Arithmetic Operator</title>
+</head>
+
+<body>
+    <h1>Number & Arithmetic Operator</h1>
+    <h2>1+1</h2>
+    <?
+        echo 1+1;
+    ?>
+    <h2>2-1</h2>
+    <?
+        echo 2-1;
+    ?>
+    <h2>2*2</h2>
+    <?
+        echo 2*2;
+    ?>
+    <h2>4/2</h2>
+    <?
+        echo 4/2;
+    ?>
+</body>
+</html>
