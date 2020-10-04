685c486818a1af79c54d2d6dac9e13126a09ef9b
okt0312 <okt0312@naver.com>
2020-10-04 17:56:16 +0900

    Variable

---
+++ php_basic/02_Variable/Variable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable</title>
+</head>
+<body>
+    <h1>Variable</h1>
+    <?
+        $name = "taek";
+        echo "Lorem ipsum dolor sit amet, 
+        consectetur ".$name."adipisicing elit. Hic ex saepe libero quis est. 
+        Quasi voluptatibus officia non temporibus unde deserunt 
+        nostrum fugit incidunt optio omnis, 
+        obcaecati tempora, soluta voluptate.";
+    ?>
+</body>
+</html>
