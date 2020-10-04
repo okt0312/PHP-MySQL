02542863df711db3c2196a397e9d73bfd3660c36
okt0312 <okt0312@naver.com>
2020-10-04 18:25:06 +0900

    URL_Parameter

---
+++ php_basic/03_URL_Parameter/Parameter.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parameter</title>
+</head>
+<body>
+    안녕하세요. <?echo $_GET['address']; ?>에 사시는 <? echo $_GET['name']; ?>님
+    <!-- /php_basic/03_URL_Parameter/Parameter.php?name=Taek&address=서울 -->
+</body>
+</html>
