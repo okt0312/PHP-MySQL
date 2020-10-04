967206c275e4cac91d02d99c06e829f9a44b3cc3
okt0312 <okt0312@naver.com>
2020-10-04 20:52:56 +0900

    Comparison_Operators

---
+++ php_basic/05_Operators/Comparison_Operators.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comparison_Operators</title>
+</head>
+<body>
+    <h1>
+        Comparison_Operators <br>
+        Boolean data type
+    </h1>
+    <h2>1==1</h2>
+    <?
+        var_dump(1==1);
+    ?>
+    <h2>1>1</h2>
+    <?
+        var_dump(1>1);
+    ?>
+    <h2>1>=1</h2>
+    <?
+        var_dump(1>=1);
+    ?>
+</body>
+</html>
