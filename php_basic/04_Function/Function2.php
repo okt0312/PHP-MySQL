7e4a1a84e38a12942fbdd9b3c20382d756fe2b73
okt0312 <okt0312@naver.com>
2020-10-06 00:19:16 +0900

    function_basic

---
+++ php_basic/04_Function/Function2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+</head>
+<body>
+    <h1>Function</h1>
+    <h2>Basic</h2>
+    <?
+        function basic()
+        {
+            print("test string1<br>");
+            print("test string2<br>");
+        }
+
+        basic();
+    ?>
+</body>
+</html>
