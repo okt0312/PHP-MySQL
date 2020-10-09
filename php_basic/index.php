39ec11cbc74cee8409c23ec928731e6145f9ab90
okt0312 <okt0312@naver.com>
2020-10-09 20:52:27 +0900

    함수로 변경

--- php_basic/index.php
+++ php_basic/index.php
@@ -1,44 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Web</title>
-</head>
-<body>
-    <h1>WEB</h1>
-    <ol>
-        <?
-            $list = scandir('data');
-            $i = 0;
-            while($i < count($list))
+<?
+    function print_title()
+    {
+        if(isset($_GET['id']))
+        {
+            echo $_GET['id']; 
+        }
+        else
+        {
+            echo "Welcome";
+        }      
+    }
+
+    function print_list()
+    {
+        $list = scandir('data');
+        $i = 0;
+        while($i < count($list))
+        {
+            if($list[$i] != '.')
             {
-                if($list[$i] != '.')
+                if($list[$i] != '..')
                 {
-                    if($list[$i] != '..')
-                    {
-        ?>
-                        <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
-        <?
-                    }
+                    echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>";
                 }
-                $i++;
             }
-        ?>
-    </ol>    
-    <h2>
-        <?
-            if(isset($_GET['id']))
-            {
-                echo $_GET['id']; 
-            }
-            else
-            {
-                echo "Welcome";
-            }
-        ?>
-    </h2>   
-    <?
+            $i++;
+        }
+    }
+
+    function print_description()
+    {
         if(isset($_GET['id']))
         {
             echo file_get_contents("data/".$_GET['id']); // 파일 불러오기
@@ -47,6 +38,26 @@
         {
             echo "Hello, PHP";
         }
-    ?>
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?print_title();?>
+    </title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <? print_list(); ?>
+    </ol>    
+    <h2>
+        <? print_title(); ?>
+    </h2>   
+    <? print_description(); ?>
 </body>
 </html>
