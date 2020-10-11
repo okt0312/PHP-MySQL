6efe0ea6333cd5767c5e114d8e3e063225d042b6
okt0312 <okt0312@naver.com>
2020-10-11 18:40:53 +0900

    mysql connect setting

---
+++ insert.php
@@ -0,0 +1,10 @@
+<?
+// https://www.php.net/manual/en/mysqli.quickstart.dual-interface.php
+
+// $mysqli = mysqli_connect("example.com", "user", "password", "database");
+// $res = mysqli_query($mysqli, "SELECT 'Please, do not use ' AS _msg FROM DUAL");
+// $row = mysqli_fetch_assoc($res);
+// echo $row['_msg'];
+
+mysqli_connect("localhost", "root", "qwe123", "opentutorials");
+?>
