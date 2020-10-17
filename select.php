9bffb77f80c29df52a476fdf82aba7c9b9be2a9f
okt0312 <okt0312@naver.com>
2020-10-17 13:40:12 +0900

    여러행 select하기

--- select.php
+++ select.php
@@ -1,10 +1,21 @@
 <?
 $conn = mysqli_connect('localhost', 'root', 'qwe123', 'opentutorials');
 
+// 1row
 $sql = "SELECT * FROM topic WHERE id = 5";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result);
 
-echo '<h1>' .$row['title']. '</h1>';
+echo '<h2>' .$row['title']. '</h2>';
 echo $row['description'];
+echo "<h1>multi row</h1>";
+
+// all row
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result))
+{
+    echo '<h2>'.$row['title'].'</h2>';
+    echo $row['description'];   
+}
 ?>
